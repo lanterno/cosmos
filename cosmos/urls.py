ea84e3eaa6ebbe0229d499c5ed606bcd5f503be6
Ahmed khan <ahmeds.elghareeb@gmail.com>
2018-11-08 03:22:56 +0200

    Adds missing URL in urls.py

--- cosmos/urls.py
+++ cosmos/urls.py
@@ -2,9 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
 from cosmos.projects.views import ProjectViewSet
+from cosmos.time_logs.views import TimeLogViewSet
+
 
 router = SimpleRouter()
 router.register(r'projects', ProjectViewSet)
+router.register(r'time_logs', TimeLogViewSet)
 
 
 urlpatterns = [
